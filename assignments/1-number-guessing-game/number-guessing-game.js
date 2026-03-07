0c52f811881852003b6f5ee81519de94f831a33d
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-03-07 17:09:37 +0100

    Random number generating logic

---
+++ assignments/1-number-guessing-game/number-guessing-game.js
@@ -0,0 +1,15 @@
+// Game settings
+const maxNumberOfGuesses = 5;
+const minNumber = 0;
+const maxNumber = 10;
+
+// Implementation taken from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values
+function getNumberToGuess() {
+  const minCeiled = Math.ceil(minNumber); // Minimum is inclusive
+  const maxFloored = Math.floor(maxNumber + 1); // Maximum is exclusive
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+for (let index = 1; index < maxNumberOfGuesses; index++) {
+  prompt(`Guess a number between ${minNumber} and ${maxNumber}`)
+}
