7dfeaec4c0c2599b0f51b379e83f65209ebb7082
Vojtech Nerad <vojtech.nerad2@gmail.com>
2026-03-30 04:14:18 +0200

    Homework 2 - file copy

--- assignments/2-file-copy/index.mjs
+++ assignments/2-file-copy/index.mjs
@@ -1,24 +1,50 @@
-import fs from 'fs';
+import { readFile, writeFile } from 'fs/promises';
 
-fs.readFile('instrukce.txt', (err, data) => {
-  if (err) {
-    console.err(err);
-    exit();
+async function getInstructions(instructionsFileName) {
+  try {
+    const instructionsFileContent = (
+      await readFile(instructionsFileName)
+    ).toString();
+
+    const [inputFileName, outputFileName] = instructionsFileContent.split(',');
+
+    return { inputFileName, outputFileName };
+  } catch (error) {
+    throw new Error('Nepodařilo se získat instrukce.', error);
   }
+}
 
-  const [inputFileName, outputFileName] = data.toString().split(',');
+async function getInput(inputFileName) {
+  try {
+    const inputFileContent = (await readFile(inputFileName)).toString();
+    return inputFileContent;
+  } catch (error) {
+    throw new Error('Nepodařilo se získat vstup.', error);
+  }
+}
 
-  fs.readFile(inputFileName, (err, data) => {
-    if (err) {
-      console.err(err);
-      exit();
-    }
+async function writeOutput(outputFileName, dataToWrite) {
+  try {
+    await writeFile(outputFileName, dataToWrite);
+  } catch (error) {
+    throw new Error('Nepodařilo se zapsat výstup.', error);
+  }
+}
 
-    console.log(data.toString());
+async function main() {
+  const { inputFileName, outputFileName } =
+    await getInstructions('instrukce.txt');
+  console.log(
+    'Získány instrukce\n',
+    `Vstupní soubor: ${inputFileName}\n`,
+    `Výstupní soubor: ${outputFileName}`,
+  );
 
-    fs.writeFile(outputFileName, data.toString(), (err) => {
-      if (err) return 'aaaa';
-      console.log('yay');
-    });
-  });
-});
+  const inputToCopy = await getInput(inputFileName);
+  console.log(`Obsah vstupního souboru: ${inputToCopy}`);
+
+  await writeOutput(outputFileName, inputToCopy);
+  console.log('Výstupní soubor s obsahem úspěšně vytvořen.');
+}
+
+main();
