3e05336f5d5e4549ff36cc28b435882bfe855494
Vojtech Nerad A895691 <vojtech.nerad@atos.net>
2026-06-26 01:50:55 +0200

    Homework 4 - query files by filename

--- coursework/4-first-program/index.js
+++ coursework/4-first-program/index.js
@@ -25,28 +25,34 @@ const server = http.createServer((request, response) => {
   const extname = String(path.extname(filePath)).toLowerCase();
   const contentType = mimeTypes[extname] || 'application/octet-stream';
 
-  try {
-    fs.readFile(filePath, (error, data) => {
-      if (error) {
-        const isFileMissing = error.code === 'ENOENT';
-        if (isFileMissing) {
-          response.writeHead(404, { 'Content-Type': 'text/plain' });
-          response.end(
-            `404 Not Found - The requested file "${fileName}" was not found on this server.`,
-          );
-        } else {
-          response.writeHead(500, { 'Content-Type': 'text/plain' });
-          response.end('500 Internal Server Error');
-        }
-      } else {
-        response.writeHead(200, { 'Content-Type': contentType });
-        response.end(data);
-      }
-    });
-  } catch (e) {
+  // If no file name is provided, serve the index.html file
+  if (!fileName) {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.end(fs.readFileSync('index.html'));
+    return;
+  }
+
+  fs.readFile(filePath, (error, data) => {
+    // If there is no error, send the file with the correct content type
+    if (!error) {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(data);
+      return;
+    }
+
+    // If the file is not found, send a 404 error with the 404.html page
+    const isFileMissing = error.code === 'ENOENT';
+    if (isFileMissing) {
+      response.writeHead(404, { 'Content-Type': 'text/html' });
+      response.end(fs.readFileSync('404.html'));
+      return;
+    }
+
+    // If there is another error, send a 500 error with a generic message
     response.writeHead(500, { 'Content-Type': 'text/plain' });
     response.end('500 Internal Server Error');
-  }
+    return;
+  });
 });
 
 server.listen(8080, 'localhost', () => {
